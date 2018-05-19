6fe50a97509af7ce1dc735fcc4ee7753a9e0fdae
liveHarshit <live.harshit@hotmail.com>
2018-05-19 11:13:13 +0530

    set cover image

--- app/src/main/java/com/example/liveharshit/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/liveharshit/booklistingapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,18 +21,42 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BookListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<BookList> bookLists = QueryUtils.extractBookList();
-        final ListView listView = (ListView)findViewById(R.id.list);
-        BookListAdapter adapter = new BookListAdapter(this,bookLists);
+        ListView listView = (ListView)findViewById(R.id.list);
+        adapter = new BookListAdapter(this,new ArrayList<BookList>());
         listView.setAdapter(adapter);
+
+//        ArrayList<BookList> bookLists = QueryUtils.extractBookList();
+
+//        BookListAdapter adapter = new BookListAdapter(this,bookLists);
+
+
+        BookListAsyncTask asyncTask = new BookListAsyncTask();
+        asyncTask.execute();
 
 
     }
 
+    private class BookListAsyncTask extends AsyncTask<Void,Void,ArrayList<BookList>> {
+
+
+        @Override
+        protected ArrayList<BookList> doInBackground(Void... voids) {
+            ArrayList<BookList> bookLists = QueryUtils.extractBookList();
+            return bookLists;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<BookList> bookLists) {
+            adapter.addAll(bookLists);
+
+        }
+    }
 
 }
