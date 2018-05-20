8a5af1d8eda882ce38c79876eeaa5a80c919e91a
liveHarshit <live.harshit@hotmail.com>
2018-05-20 14:49:17 +0530

    add splash screen and icon

---
+++ app/src/main/java/com/example/liveharshit/booklistingapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.liveharshit.booklistingapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toMainActivity = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(toMainActivity);
+                finish();
+            }
+        },SPLASH_SCREEN_TIMEOUT);
+    }
+}
